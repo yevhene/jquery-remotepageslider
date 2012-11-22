50efdc2a40c06d8d068afe0494fc9907e0f4044c
Yevhene Shemet <yevhene@gmail.com>
2012-11-22 17:41:26 +0200

    Synchronous insertion and transition implemented.

--- contentslider.js
+++ contentslider.js
@@ -1,21 +1,114 @@
 (function($) {
-    $.fn.contentSlider = function(options) {
-        var _ = $(this);
-        var _ul = $(this).find('ul');
-        var cfg = {
-            width: _.width(),
-            height: _.height(),
-            index: 0
-        };
-        $.extend(cfg, options);
+    var LI_CSS = {
+        'float': 'left',
+        'list-style': 'none',
+        'margin': '0',
+        'padding': '0',
+        'position': 'relative',
+        'height': '100%'
+    };
 
-        var currentIndex = cfg.index;
+    var CONFIG_DEFAULTS = {
+        index: 0,
+        loadRange: 1,
+        getPage: function(index) {
+            return null;
+        },
+        hasIndex: function(index) {
+            return false;
+        }
+    };
+
+    $.fn.contentSlider = function(options) {
+        var _config = {};
+        $.extend(_config, CONFIG_DEFAULTS);
+        $.extend(_config, options);
+
+        var _currentIndex = _config.index;
+
+        var _root = $(this);
+        var _container;
+        var _ul;
+
+        var firstLiWithIndex = function(index) {
+            var result = null;
+            _ul.find('li').each(function() {
+                if ($(this).data('index') === index) {
+                    result = $(this);
+                    return false;
+                }
+            });
+            return result;
+        };
+
+        var findLiBeforeIndex = function(index) {
+            var result = null;
+            _ul.find('li').each(function(){
+                if ($(this).data('index') > index) {
+                    return false;
+                }
+                result = $(this);
+            });
+            return result;
+        };
+
+        var lock = {
+            show: false
+        };
+        var show = function(index, animation) {
+            if (animation === undefined) {
+                animation = 0;
+            }
+            if (lock.show) {
+                return;
+            }
+            lock.show = true;
+            if (!_config.hasIndex(index)) {
+                return;
+            }
+            var leftIndex = index - _config.loadRange;
+            var rightIndex = index + _config.loadRange;
+            var mainLi;
+            var i = 0;
+            for (i = leftIndex; i <= rightIndex; ++i) {
+                if (_config.hasIndex(i)) {
+                    var li = firstLiWithIndex(i);
+                    if (!li) {
+                        var page = _config.getPage(i);
+                        li = $('<li></li>');
+                        li.data('index', i);
+                        li.css(LI_CSS);
+                        li.append(page);
+                        var liBeforeI = findLiBeforeIndex(i);
+                        if (liBeforeI) {
+                            liBeforeI.after(li);
+                        } else {
+                            _ul.append(li);
+                        }
+                    }
+                    if (i === index) {
+                        mainLi = li;
+                    }
+                }
+            }
+
+            var mainLiHCenter = mainLi.position().left + mainLi.width() / 2;
+            var hPosition = _container.width() / 2 - mainLiHCenter;
+            _ul.animate({
+                left: hPosition
+            }, animation, function() {
+                lock.show = false;
+            });
+        };
 
         (function init() {
-            _.css({
+            _container = $('<div></div>');
+            _container.css({
                 'overflow': 'hidden',
                 'position': 'relative'
             });
+            _root.append(_container);
+            _ul = $('<ul></ul>');
             _ul.css({
                 'overflow': 'visible',
                 'width': '999999px',
@@ -24,45 +117,17 @@
                 'padding': '0',
                 'position': 'relative'
             });
-            _ul.children().each(function() {
-                $(this).width(cfg.width);
-                $(this).height(cfg.height);
-                $(this).css({
-                    'float': 'left',
-                    'list-style': 'none',
-                    'margin': '0',
-                    'padding': '0',
-                    'position': 'relative'
-                });
-            });
+            _container.append(_ul);
+            show(_currentIndex);
         }());
 
-        var moveLock = false;
-        var move = function(left) {
-            if (moveLock) {
-                return;
-            }
-            moveLock = true;
-            _ul.animate({
-                left: left
-            }, 'slow', function() {
-                moveLock = false;
-            });
-        };
-
-        var prev = function() {
-            var left = _ul.position().left + cfg.width;
-            move(left);
-        };
-
-        var next = function() {
-            var left = _ul.position().left - cfg.width;
-            move(left);
+        var index = function() {
+            return _currentIndex;
         };
 
         return {
-            next: next,
-            prev: prev
+            show: show,
+            index: index
         };
     };
 }(jQuery));
